e721d6bc06abb350de167b99b206c83a430eabee
Hao Jin <haoj@andrew.cmu.edu>
2017-11-09 19:51:14 -0500

    change to grayscale

--- sample_nn.py
+++ sample_nn.py
@@ -4,6 +4,7 @@ from keras.models import Sequential
 from keras.layers.core import Flatten, Dense, Dropout
 from keras.layers.convolutional import Convolution2D, MaxPooling2D
 from keras.optimizers import SGD
+from skimage.color import rgb2gray
 import numpy as np
 import sys
 #https://gist.github.com/baraldilorenzo/07d7802847aaad0a35d3
@@ -19,16 +20,18 @@ def main(argv):
   x = []
   y = []
   for i in range(num_train):
-    x.append(all_data[i][1][0])
+    x.append(rgb2gray(all_data[i][1][0]))
     label = np.zeros((49,), dtype=float)
     label[l_dict[str(all_data[i][0])][0]] = 1.
     y.append(label)
+  image_shape = x[0].shape
+  print image_shape
   x = np.asarray(x)
   y = np.asarray(y)
   print x.shape
   print y.shape
   model = Sequential()
-  model.add(Convolution2D(64,3,3,activation ='relu', input_shape=(180,180,3)))
+  model.add(Convolution2D(32,3,3,activation ='relu', input_shape=(180, 180, 1)))
   model.add(MaxPooling2D((2,2), strides=(2,2)))
   # model.add(Convolution2D(128,3,3, activation ='relu'))
   # model.add(MaxPooling2D((2,2), strides=(2,2)))
@@ -39,7 +42,7 @@ def main(argv):
 
   sgd = SGD(lr=0.1, decay=1e-6, momentum=0.5)
   model.compile(optimizer=sgd, loss='categorical_hinge')
-  model.fit(x, y, batch_size=100, epochs=200, verbose=1)
+  model.fit(np.reshape(x[:, :, :], (num_train, 180, 180, 1)), y[:, :], epochs=200, verbose=1)
 
 if __name__ == '__main__':
   main(sys.argv)
